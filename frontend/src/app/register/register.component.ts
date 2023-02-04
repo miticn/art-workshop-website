bc5416388ebc23622d2a29864dc8dc15481adf0a
miticn <101733519+miticn@users.noreply.github.com>
2023-02-04 20:03:48 +0100

    File format check works

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -25,7 +25,7 @@ export class RegisterComponent implements OnInit {
     email: new FormControl('', [Validators.required, Validators.email], [emailValidator(this.service)]),
     password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16), Validators.pattern("^(.*[a-zA-Z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])[a-zA-Z0-9!@#\$%\^&\*]+$")]),
     confirmPassword: new FormControl('', [Validators.required]),
-    profilePicture: new FormControl('', []),
+    profilePicture: new FormControl('', [Validators.pattern("^.+\.(jpg|png|jpeg|JPG|PNG|JPEG)$")]),
     profilePicutreFile: new FormControl(null, []),
     imageDimensionsX: new FormControl(100, [Validators.max(300), Validators.min(100)]),
     imageDimensionsY: new FormControl(100, [Validators.max(300), Validators.min(100)]),
@@ -45,15 +45,14 @@ export class RegisterComponent implements OnInit {
   organizer:boolean;
   @ViewChild('canvas', { static: false }) canvas
   onFileChange(event) {
-    console.log(this.registerForm.controls);
-    console.log("Register form validation: ", this.registerForm.valid);
-    
+    //console.log(this.registerForm.controls);
+    //console.log("Register form validation: ", this.registerForm.valid);
+    //alert(this.registerForm.controls.profilePicture.valid)
     if (event.target.files.length == 0){
       this.registerForm.controls.imageDimensionsX.setValue(100);
       this.registerForm.controls.imageDimensionsY.setValue(100);
     }
-
-    if (event.target.files.length > 0) {
+    else if (event.target.files.length > 0 && this.registerForm.controls.profilePicture.valid) {
       const file = event.target.files[0];
       this.registerForm.patchValue({
         profilePicutreFile: file
@@ -71,6 +70,10 @@ export class RegisterComponent implements OnInit {
         console.log('Image dimensions:', this.registerForm.value.imageDimensionsX, this.registerForm.value.imageDimensionsY);
         console.log(this.registerForm.controls.imageDimensionsX.errors)
       };
+    }
+    if(!this.registerForm.controls.profilePicture.valid){
+      this.registerForm.controls.imageDimensionsX.setValue(100);
+      this.registerForm.controls.imageDimensionsY.setValue(100);
     }
   }
 
